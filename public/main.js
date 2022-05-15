0cc89128c5d663ffbb654c4a070666970860b0ce
Dan Govier <dan.govier@gmail.com>
2022-05-14 19:00:40 -0500

    switching to realtime database

--- public/main.js
+++ public/main.js
@@ -1,24 +1,21 @@
 // Each firebase document is effectively a day
 // A document's collections are individual entries
+// const app = firebase.app;
+// const db = firebase.firestore();
+
+const entriesRef = firebase.database().ref("entries/");
 
 document.addEventListener("DOMContentLoaded", (event) => {
-  const app = firebase.app();
-
-  const db = firebase.firestore();
-
   // TODO: replace with myEntries = db.entries (get all of the entries as a single collection)
-  const myPost = db.collection("entries").doc("s1G11znw5wKjuP2rky9W");
-
+  // const myPost = db.collection("entries").doc("s1G11znw5wKjuP2rky9W");
   // TODO: create an input div for today, input disabled unless user is logged in
-
   // TODO: replace with iteration through the collection's entries
   // TODO: and creating a row of div(s) for each collection/day
-  myPost.get().then((doc) => {
-    const data = doc.data();
-
-    document.querySelector("#entry").innerHTML =
-      data.content + `<br>` + Date(data.createdOn);
-  });
+  // myPost.get().then((doc) => {
+  //   const data = doc.data();
+  //   document.querySelector("#entry").innerHTML =
+  //     data.content + `<br>` + Date(data.createdOn);
+  // });
 });
 
 function googleLogin() {
@@ -42,9 +39,17 @@ function updatePost(e) {
   // check to see if an entry exists for this day
   // if so, append this collection to that document
   // if not, create a new document with this as its first collection
-  const db = firebase.firestore();
-  const myPost = db.collection("entries").doc("s1G11znw5wKjuP2rky9W");
-  myPost.update({ content: document.getElementById("newEntry").value });
+  // const myPost = db.collection("entries").doc("s1G11znw5wKjuP2rky9W");
+  entriesRef.push({
+    timestamp: Date.now(),
+    entry: document.getElementById("newEntry").value,
+  });
+  // entriesRef.set ({
+  //   first: {
+  //     entry: document.getElementById("newEntry").value
+  //   }
+  // });
+  // myPost.update({ content: document.getElementById("newEntry").value });
 }
 
 // TODO: function to create entries for a day/collection
